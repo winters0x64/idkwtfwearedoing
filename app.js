f0d64336f76748147d30eb1af081e222c2a595db
Luc1f3r0001 <adithyaraj2515@gmail.com>
2023-07-28 23:52:09 +0000

    Only ui and the follower bug remains

--- app.js
+++ app.js
@@ -26,31 +26,55 @@ const db = new sqlite3.Database('database.db', (err) => {
     }
 });
 // Need table for blog too
-db.run(`
-  CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL
-  );
+let database = new Promise(function(data,error) {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT NOT NULL,
+      password TEXT NOT NULL,
+      followers INTEGER DEFAULT 0,
+      following INTEGER DEFAULT 0
+    );
 `);
 db.run(`
   CREATE TABLE IF NOT EXISTS blog (
+  blog_id INTEGER PRIMARY KEY AUTOINCREMENT,
   id INTEGER,
-  blog_id INTEGER,
   name TEXT NOT NULL,
   content TEXT NOT NULL,
-  PRIMARY KEY (id,blog_id)
+  FOREIGN KEY (id) REFERENCES users(id)
   );
 `);
+})
 
-db.run('INSERT INTO users (username, password) VALUES (?, ?)', ["Bob", "Bob"], function (err) {
+/* database.then(
+  function(value) {
+  db.run('INSERT INTO users (username, password) VALUES (?, ?)', ["Bob", "Bob"], function (err) {
     if (err) {
       console.error('Error inserting data:', err.message);
     } 
     else {
       console.log(`User "Bob" registered successfully.`);
     }
-});
+});}
+) */
+
+db.get('select id from users where username = ?',["bob"],function(err,data) {
+  if (err) {
+    db.run('INSERT INTO users (username, password) VALUES (?, ?)', ["bob", "bob"], function (err) {
+      if (err) {
+        console.error('Error inserting data:', err.message);
+      } 
+      else {
+        console.log(`User "bob" registered successfully.`);
+      }
+  });
+  } else {
+    console.log('bob exists');
+  }
+})
+
+
 
 
 // Routes
@@ -92,8 +116,8 @@ app.post('/login', (req, res) => {
       else {
         if (row) {
           req.session.username = row.username;
-          req.session.id = row.id;
-          res.redirect('/user');
+          req.session.num = row.id;
+          res.redirect('/feed');
         } 
         else {
           res.send('Invalid login credentials.');
@@ -104,24 +128,111 @@ app.post('/login', (req, res) => {
 
 
 // Start working from here
-app.get('/user', (req, res) => {
-  if(!req.session.id && !req.session.username){
+app.get('/user/:username', (req, res) => {
+  if(!req.session.num && !req.session.username){
     res.redirect('/login');
   } else {
-    const username = req.session.username ;
-    const id = req.session.id;
-    db.all('SELECT * FROM blog WHERE id = ?',[id],(err,row) => {
+    const username = req.params.username;
+    const id_num = req.session.num
+    db.get('SELECT id,followers,following from users WHERE username = ?',[username],(err,data) => {
       if(err) {
-      res.render('profile', { username : username,row:row });
-    } else {
-      res.render('profile', { username : username,row:row});
-    }
+        res.send('No user found');
+      } else if (req.params.username === req.session.username) {
+        res.redirect(`/profile/${username}`)
+      } else {
+        let id = data.id
+        db.all('SELECT * FROM blog WHERE id = ?',[id],(err,row) => {
+          if(err) {
+          res.render('user', { data,username,row });
+        } else {
+          console.log(`Followers = ${data.followers} Following = ${data.following}`)
+          res.render('user', { data,username,row});
+          console.log(`name ${username} id = ${id_num}`)
+        }
+        })
+      }
+    })
+  }
+});
+
+app.get('/profile/:username', (req, res) => {
+  if(!req.session.num && !req.session.username){
+    res.redirect('/login');
+  } else if (req.params.username !== req.session.username) {
+    res.send('You are not authorized to view this page.');
+  } else {
+    const username = req.params.username;
+    const id = req.session.num;
+    db.get('SELECT followers,following from users WHERE id = ?',[id],(err,data) => {
+      if(err) {
+        res.send('No user found');
+      } else {
+        db.all('SELECT * FROM blog WHERE id = ?',[id],(err,row) => {
+          if(err) {
+          res.render('profile', { data,username,row });
+        } else {
+          console.log(`Followers = ${data.followers} Following = ${data.following}`)
+          res.render('profile', { data,username,row});
+          console.log(`name ${username} id = ${id}`)
+        }
+        })
+      }
+    })
+  }
+});
+
+app.post('/follow/:username', (req, res) => {
+  if (!req.session.num || !req.session.username) {
+    res.redirect('/login');
+  } else {
+    const loggedInUser = req.session.username;
+    const targetUsername = req.params.username;
+    console.log(`${loggedInUser} of ${targetUsername}`)
+
+    // Check if the logged-in user and target user exist in the database
+    db.get('SELECT id FROM users WHERE username = ?', [loggedInUser], (err, loggedInUserRow) => {
+      if (err) {
+        console.error('Error querying the database:', err.message);
+        res.status(500).send('Error querying the database.');
+      } else {
+        db.get('SELECT id FROM users WHERE username = ?', [targetUsername], (err, targetUserRow) => {
+          if (err) {
+            console.error('Error querying the database:', err.message);
+            res.status(500).send('Error querying the database.');
+          } else {
+            console.log(`target user:= ${targetUserRow.id} || logged user := ${loggedInUserRow.id}`)
+            if (!targetUserRow || !loggedInUserRow) {
+              res.status(404).send('User not found.');
+            } else {
+              // Update the follower and following counts for both users
+              const targetUserId = targetUserRow.id
+              const loggedInUserId = loggedInUserRow.id
+
+              db.run('UPDATE users SET followers = followers + 1 WHERE id = ?', [targetUserId], (err) => {
+                if (err) {
+                  console.error('Error updating follower count:', err.message);
+                }
+              });
+
+              db.run('UPDATE users SET following = following + 1 WHERE id = ?', [loggedInUserId], (err) => {
+                if (err) {
+                  console.error('Error updating following count:', err.message);
+                }
+              });
+
+              res.redirect(`/user/${targetUsername}`);
+            }
+          }
+        });
+      }
     });
   }
-  });
+});
 
-app.post('/blog',(req,res) => {
-  const id = req.session.id
+
+app.post('/blog/:username',(req,res) => {
+  const id = req.session.num
+  const name = req.params.username
   const {title,content} = req.body
   console.log(`The Blog is ${title} and ${content}`)
   db.run(`INSERT INTO blog (id,name,content) VALUES (?,?,?)`,[id,title,content],(err,row) => {
@@ -131,10 +242,33 @@ app.post('/blog',(req,res) => {
     } 
     else {
       console.log(`Blog written successful`);
-      res.redirect('/user');
+      res.redirect(`/profile/${name}`);
     }
-  });
+  })
 });
+
+app.get('/feed',(req,res) => {
+  if(!req.session.num && !req.session.username){
+    res.redirect('/login');
+  } else {
+    db.all('SELECT * FROM blog',(err,row) => {
+      if(err) {
+      res.status(500).send("Error Loading the feed ");
+    } else {
+      db.all('SELECT * FROM users ',(err,user) => {
+        if (err) {
+          console.error('Error Loading the users.', err.message);
+          res.status(500).send('Error Loading the users.');
+        } 
+        else {
+          res.render('feed', { user :user,row:row});
+        }
+      })
+    }
+  })
+}
+});
+  
 
 app.use(express.static('public'));
 
